acdb9e5d68c348ace57fc9de785d6420a81af678
Ziyi31-byte <79799664+Ziyi31-byte@users.noreply.github.com>
2023-05-16 13:17:24 +0800

    Update longest-consecutive-sequence.py

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -1,3 +1,18 @@
+# 437ms, beats 49%
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        res_default = 0
+        for i in s:
+            if i-1 not in s:
+                j = i+1
+                res = 1
+                while j in s:
+                    res += 1
+                    j += 1
+                res_default = max(res,res_default)
+        return res_default
+    
 # 491ms, beats 47%
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
